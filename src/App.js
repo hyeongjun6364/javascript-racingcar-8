930055995e7bbb21d91c0d298762aa56455262fe
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-24 00:46:32 +0900

    feat: 자동차 이름과 시도할 횟수 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from '@woowacourse/mission-utils';
+import { MESSAGES } from './constants.js';
+
 class App {
-  async run() {}
+  #cars;
+  #tryCount;
+  async run() {
+    const carNames = await this.userInput(MESSAGES.userInput);
+    const count = await this.userInput(MESSAGES.userInputTryCount);
+  }
+
+  async userInput(content) {
+    return await Console.readLineAsync(content);
+  }
 }
 
 export default App;
