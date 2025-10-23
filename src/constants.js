4313ff3af1bde06a255330bdfe78114a7e3287d8
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-24 00:46:55 +0900

    feat: 안내 메세지 상수화 적용

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const MESSAGES = Object.freeze({
+  userInput: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)로 구분).\n',
+  userInputTryCount: '시도할 횟수는 몇 회인가요?\n',
+  printRunResult: '실행 결과\n',
+  printFinalWinner: '최종 우승자: ',
+});
