65d2531899591c8d18fb97190b26e11d8f626829
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-24 18:39:22 +0900

    feat: 시도 횟수동안 자동차 전진기능 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 import { MESSAGES } from './constants.js';
+import Car from './Car.js';
 
 class App {
   #cars;
   #tryCount;
+
   async run() {
     const carNames = await this.userInput(MESSAGES.userInput);
-    const count = await this.userInput(MESSAGES.userInputTryCount);
+    const tryCount = await this.userInput(MESSAGES.userInputTryCount);
+    this.setCars(carNames);
+    this.setTryCount(tryCount);
+    this.startRacing();
+  }
+
+  startRacing() {
+    for (let i = 0; i < this.#tryCount; i++) {
+      this.moveCars();
+    }
+  }
+
+  moveCars() {
+    this.#cars.forEach((car) => {
+      car.move();
+    });
   }
 
   async userInput(content) {
     return await Console.readLineAsync(content);
+  }
+
+  printResult(content) {
+    Console.print(content);
+  }
+
+  setCars(carNames) {
+    this.#cars = carNames.split(',').map((name) => new Car(name.trim()));
+  }
+
+  setTryCount(count) {
+    this.#tryCount = count;
   }
 }
 
