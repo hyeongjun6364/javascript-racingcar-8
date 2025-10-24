2873098a1f7065c9e429f4a72d741b319c6fbd15
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-24 18:53:30 +0900

    refactor: 필요없는 문자 삭제

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const MESSAGES = Object.freeze({
   userInput: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)로 구분).\n',
   userInputTryCount: '시도할 횟수는 몇 회인가요?\n',
-  printRunResult: '\n실행 결과\n',
+  printRunResult: '\n실행 결과',
   printFinalWinner: '최종 우승자: ',
 });
 
