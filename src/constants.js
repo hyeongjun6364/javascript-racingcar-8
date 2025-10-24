e39aca3f60eb472e4434f77a026341a8a95c7586
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-24 18:15:26 +0900

    feat: 자동차 전진 조건에 대한 범위와 전진 기준 상수화

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,10 @@
 export const MESSAGES = Object.freeze({
   userInput: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)로 구분).\n',
   userInputTryCount: '시도할 횟수는 몇 회인가요?\n',
-  printRunResult: '실행 결과\n',
+  printRunResult: '\n실행 결과\n',
   printFinalWinner: '최종 우승자: ',
 });
+
+export const MIN_NUMBER_RANGE = 0;
+export const MAX_NUMBER_RANGE = 9;
+export const MOVE_CONDITION_NUMBER = 4;
