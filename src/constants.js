84bd8439f399e73f1a16797a1ae28d9da3c8bd7e
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-24 19:40:21 +0900

    feat: 에러 메세지 상수화

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,7 @@
+export const MIN_NUMBER_RANGE = 0;
+export const MAX_NUMBER_RANGE = 9;
+export const MOVE_CONDITION_NUMBER = 4;
+
 export const MESSAGES = Object.freeze({
   userInput: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   userInputTryCount: '시도할 횟수는 몇 회인가요?\n',
@@ -5,6 +9,6 @@ export const MESSAGES = Object.freeze({
   printFinalWinner: '최종 우승자 : ',
 });
 
-export const MIN_NUMBER_RANGE = 0;
-export const MAX_NUMBER_RANGE = 9;
-export const MOVE_CONDITION_NUMBER = 4;
+export const ERROR_MESSAGES = Object.freeze({
+  INVALID_CAR_NAME: '[ERROR] 자동차 이름은 1자 이상 5자 이하이어야 합니다.',
+});
