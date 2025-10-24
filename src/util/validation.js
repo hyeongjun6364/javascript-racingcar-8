26de0e9af11ac2f42364cef1ede8f79ac25bf007
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-24 19:49:17 +0900

    feat: 자동차의 이름이 빈문자열일 경우 유효성 처리

--- src/util/validation.js
+++ src/util/validation.js
@@ -4,10 +4,14 @@ export const carNameValidation = (userInput) => {
   const carNames = userInput.split(',').map((name) => name.trim());
 
   const isValidCarName = carNames.every((name) => name.length <= 5 && name.length > 0);
+  const hasEmptyName = carNames.some((name) => name === '');
   const hasDuplicateNames = new Set(carNames).size !== carNames.length;
   const isValidLength = carNames.length >= 2;
   if (hasDuplicateNames) {
     throw new Error(ERROR_MESSAGES.DUPLICATE_CAR_NAME);
+  }
+  if (hasEmptyName) {
+    throw new Error(ERROR_MESSAGES.EMPTY_CAR_NAME);
   }
 
   if (!isValidLength) {
