b5fa6a1fa232b8383e3cd857c8ef0cb7d2fb9f60
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-24 19:06:43 +0900

    refactor: 안내 메세지 수정

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 export const MESSAGES = Object.freeze({
-  userInput: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)로 구분).\n',
+  userInput: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   userInputTryCount: '시도할 횟수는 몇 회인가요?\n',
   printRunResult: '\n실행 결과',
-  printFinalWinner: '최종 우승자: ',
+  printFinalWinner: '최종 우승자 : ',
 });
 
 export const MIN_NUMBER_RANGE = 0;
