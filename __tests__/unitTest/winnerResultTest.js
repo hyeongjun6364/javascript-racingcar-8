884c22a2d3e7777d596b3ac25f22baddbcbaab5e
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-27 15:14:21 +0900

    refactor: 우승자 선정 로직 테스트 race 모델 적용

--- __tests__/unitTest/winnerResultTest.js
+++ __tests__/unitTest/winnerResultTest.js
@@ -1,31 +1,29 @@
-import App from '../../src/App';
+import Race from '../../src/Race';
 
 describe('우승자 선정 로직 테스트', () => {
   test('가장 멀리 간 자동차를 우승자로 선정한다', () => {
-    const app = new App();
-    app.setCars('pobi,crong,honux');
+    const race = new Race('pobi,crong,honux', 5);
 
-    const cars = app.getCars();
+    const cars = race.getCars();
     cars[0].distance = 3;
     cars[1].distance = 5;
     cars[2].distance = 2;
 
-    const winners = app.getWinners();
+    const winners = race.getWinners();
 
     expect(winners.length).toBe(1);
     expect(winners[0].getName()).toBe('crong');
   });
 
   test('공동 우승자가 여러 명일 수 있다', () => {
-    const app = new App();
-    app.setCars('pobi,crong,honux');
+    const race = new Race('pobi,crong,honux', 5);
 
-    const cars = app.getCars();
+    const cars = race.getCars();
     cars[0].distance = 5;
     cars[1].distance = 5;
     cars[2].distance = 3;
 
-    const winners = app.getWinners();
+    const winners = race.getWinners();
 
     expect(winners.length).toBe(2);
     expect(winners[0].getName()).toBe('pobi');
